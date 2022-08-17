d90f7cf83d835c591aee73d30e083f8a75dfb2ed
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2022-08-17 18:41:07 -0300

    added seventh portfolio (fix imgs path later)

--- src/components/sections/portfolioSection/index.jsx
+++ src/components/sections/portfolioSection/index.jsx
@@ -1,55 +1,63 @@
-import React from "react"
-import "./portfolio-section.css"
-import portfolioOne from "../../../assets/screenshot-event-platform.png"
-import portfolioTwo from "../../../assets/screenshot-feedback-widget.png"
-import portfolioThree from "../../../assets/screenshot-image-uploader.png"
-import portfolioFour from "../../../assets/screenshot-github-explorer.png"
-import portfolioFive from "../../../assets/ignite-feed-screenshot.png"
-import portfolioSix from "../../../assets/modern-ui-todo-app-screenshot.png"
+import React from 'react'
+import './portfolio-section.css'
+import portfolioOne from '../../../assets/screenshot-event-platform.png'
+import portfolioTwo from '../../../assets/screenshot-feedback-widget.png'
+import portfolioThree from '../../../assets/screenshot-image-uploader.png'
+import portfolioFour from '../../../assets/screenshot-github-explorer.png'
+import portfolioFive from '../../../assets/ignite-feed-screenshot.png'
+import portfolioSix from '../../../assets/modern-ui-todo-app-screenshot.png'
+import portfolioSeven from '../../../assets/coffe-delivery-demo-app.png'
 
 const portfolioData = [
   {
     id: 1,
     image: portfolioOne,
-    github: "https://github.com/lucaspedroabreu/event-plataform-app",
-    demo: "https://event-plataform-app.vercel.app/",
-    title: "Event Plataform Application with lessons schedule and CMS",
+    github: 'https://github.com/lucaspedroabreu/event-plataform-app',
+    demo: 'https://event-plataform-app.vercel.app/',
+    title: 'Event Plataform Application with lessons schedule and CMS',
   },
   {
     id: 2,
     image: portfolioTwo,
-    github: "https://github.com/lucaspedroabreu/feedback-widget",
-    demo: "https://feedback-widget-two-pied.vercel.app/",
-    title: "Feedback Widget with Vite App",
+    github: 'https://github.com/lucaspedroabreu/feedback-widget',
+    demo: 'https://feedback-widget-two-pied.vercel.app/',
+    title: 'Feedback Widget with Vite App',
   },
   {
     id: 3,
     image: portfolioThree,
-    github: "https://github.com/lucaspedroabreu/image-uploader-backend",
-    demo: "https://lpa-image-uploader.herokuapp.com/",
+    github: 'https://github.com/lucaspedroabreu/image-uploader-backend',
+    demo: 'https://lpa-image-uploader.herokuapp.com/',
     title:
-      "A simple single responsability app with complete functionality: Uploads images to a remote storage and lets you fetch it or remove it.",
+      'A simple single responsability app with complete functionality: Uploads images to a remote storage and lets you fetch it or remove it.',
   },
   {
     id: 4,
     image: portfolioFour,
-    github: "https://github.com/lucaspedroabreu/github_explorer",
-    demo: "https://github-explorer-rust-rho.vercel.app/",
-    title: "A simple Github explorer web application",
+    github: 'https://github.com/lucaspedroabreu/github_explorer',
+    demo: 'https://github-explorer-rust-rho.vercel.app/',
+    title: 'A simple Github explorer web application',
   },
   {
     id: 4,
     image: portfolioFive,
-    github: "https://github.com/lucaspedroabreu/feed-page-example",
-    demo: "https://feed-page-example.vercel.app/",
-    title: "A Mocking Social Feed Page",
+    github: 'https://github.com/lucaspedroabreu/feed-page-example',
+    demo: 'https://feed-page-example.vercel.app/',
+    title: 'A Mocking Social Feed Page',
   },
   {
     id: 5,
     image: portfolioSix,
-    github: "https://github.com/lucaspedroabreu/mordern-ui-todos-list",
-    demo: "https://mordern-ui-todos-list.vercel.app/",
-    title: "A modern UI Todo List App",
+    github: 'https://github.com/lucaspedroabreu/mordern-ui-todos-list',
+    demo: 'https://mordern-ui-todos-list.vercel.app/',
+    title: 'A modern UI Todo List App',
+  },
+  {
+    id: 6,
+    image: portfolioSeven,
+    github: 'https://github.com/lucaspedroabreu/coffee-delivery-e-commerce',
+    demo: 'https://coffee-delivery-e-commerce.vercel.app/',
+    title: 'A incomplete e-commerce app for coffee delivery',
   },
 ]
 
@@ -67,20 +75,10 @@ function Portfolio() {
             </div>
             <h3>{item.title}</h3>
             <div className="portfolio__item-cta">
-              <a
-                href={item.github}
-                className="btn"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
+              <a href={item.github} className="btn" target="_blank" rel="noreferrer noopener">
                 GitHub
               </a>
-              <a
-                href={item.demo}
-                className="btn btn-primary"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
+              <a href={item.demo} className="btn btn-primary" target="_blank" rel="noreferrer noopener">
                 Live Demo
               </a>
             </div>
